36cacb5399c08c51fa91c15fd3e726de2653febd
immortal mind <immortal.mind2016@gmail.com>
2018-10-27 16:23:28 +0200

    deployed

---
+++ build/precache-manifest.78103f51171789e8661b49e465ab2696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8026036858ecbae61e8a",
+    "url": "/static/js/main.80260368.chunk.js"
+  },
+  {
+    "revision": "6321f4bad6ad4ddcca18",
+    "url": "/static/js/1.6321f4ba.chunk.js"
+  },
+  {
+    "revision": "8026036858ecbae61e8a",
+    "url": "/static/css/main.cebe44e0.chunk.css"
+  },
+  {
+    "revision": "a545dd1cb86a9c9655b2949052662311",
+    "url": "/index.html"
+  }
+];
